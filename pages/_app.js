146c2dd4c5b071d418bd65dc032440bc5533e9ee
Guillermo Martinez <gmartinez948@outlook.com>
2021-12-09 12:46:08 -0500

    boiler plate file structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,7 @@
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  styled,
-  Box,
-  // alpha,
-  // InputBase,
-} from "@mui/material";
 import Link from "next/link";
-import SearchBar from "../components/SearchBar.js";
 
 const App = ({ Component, pageProps }) => {
-  return (
-    <div>
-      <AppBar
-        position="static"
-        color="secondary"
-        sx={{ backgroundColor: "lightblue" }}
-      >
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Products
-          </Typography>
-          <div>
-            <SearchBar />
-          </div>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Link href="/products">
-        <a> products </a>
-      </Link>
-      <Link href="/">
-        <a> Home</a>
-      </Link>
-      <h1>this is on every page</h1>
-      <Component {...pageProps} />
-    </div>
-  );
+  return <div>Hey</div>;
 };
 
 export default App;
--- pages/products/[productId].js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Turkey() {
-  const router = useRouter();
-  console.log(router);
-  return <div>Turkey</div>;
-}
