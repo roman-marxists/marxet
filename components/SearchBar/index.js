f5117d9436155cbdfaa3e23fbbd9e4dc8992cd71
Guillermo Martinez <gmartinez948@outlook.com>
2021-12-09 19:59:20 -0500

    made some changes to header

---
+++ components/SearchBar/index.js
@@ -0,0 +1,44 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  styled,
+  alpha,
+  Box,
+  InputBase,
+  TextField,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(searchTerm);
+  };
+
+  return (
+    <Box>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          onChange={handleChange}
+          variant="standard"
+          label="Search for Products"
+        />
+        {/* <Typography /> */}
+        <Button type="submit" variant="contained" color="primary">
+          Search
+        </Button>
+      </form>
+    </Box>
+  );
+};
+
+export default Search;
--- pages/_app.js
+++ pages/_app.js
@@ -1,43 +1,24 @@
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  styled,
-  Box,
-  // alpha,
-  // InputBase,
-} from "@mui/material";
+import { Box } from "@mui/material";
+
 import Link from "next/link";
-import SearchBar from "../components/SearchBar.js";
+
+import SearchBar from "../components/SearchBar";
+import Header from "../components/Header.js";
+
+import { useState } from "react";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <div>
-      <AppBar
-        position="static"
-        color="secondary"
-        sx={{ backgroundColor: "lightblue" }}
-      >
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Products
-          </Typography>
-          <div>
-            <SearchBar />
-          </div>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Link href="/products">
-        <a> products </a>
-      </Link>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Header />
+      </Box>
       <Link href="/">
-        <a> Home</a>
+        <a>Home</a>
       </Link>
       <h1>this is on every page</h1>
       <Component {...pageProps} />
-    </div>
+    </>
   );
 };
 
