aca79e48025c0a9425126ec304a4972ddc6b4f72
Kelly <kellytso04@gmail.com>
2021-12-10 11:37:48 -0500

    Creates exported Theme for app

---
+++ components/Theme.js
@@ -0,0 +1,30 @@
+import { createTheme } from '@mui/material';
+
+export default const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ffffff',
+      main: '#c7d6d5',
+      dark: '#667574',
+      contrastText: '#667574'
+    },
+    secondary: {
+      light: '#ffffff',
+      main: '#801f12',
+      dark: '#343a40',
+      contrastText: '#ffffff'
+    },
+    info: {
+      main: '#dbd5d5'
+    },
+    success: {
+      main: '#d1e6e4'
+    },
+    error: {
+      main: '#c62828'
+    },
+    warning: {
+      main: '#801f12'
+    }
+  }
+});
