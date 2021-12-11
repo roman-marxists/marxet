bd12af5b561a76f3319d6cddcf2f0c22ca00d0b6
Kelly <kellytso04@gmail.com>
2021-12-11 13:53:45 -0500

    Wraps App in ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,43 @@
 import { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, createTheme, ThemeProvider } from "@mui/material";
 import Link from "next/link";
 
 import '../public/styles.css';
 import SearchBar from "../components/SearchBar";
 import Header from "../components/Header.js";
 
+const Theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ffffff',
+      main: '#c7d6d5',
+      dark: '#667574',
+      contrastText: '#667574'
+    },
+    secondary: {
+      light: '#ffffff',
+      main: '#801f12',
+      dark: '#343a40',
+      contrastText: '#ffffff'
+    },
+    info: {
+      main: '#dbd5d5'
+    },
+    success: {
+      main: '#d1e6e4'
+    },
+    error: {
+      main: '#c62828'
+    },
+    warning: {
+      main: '#801f12'
+    }
+  }
+});
 
 const App = ({ Component, pageProps }) => {
   return (
-    <>
+    <ThemeProvider theme={Theme}>
       <Box sx={{ flexGrow: 1 }}>
         <Header />
       </Box>
@@ -17,7 +45,7 @@ const App = ({ Component, pageProps }) => {
         <a>Home</a>
       </Link>
       <Component {...pageProps} />
-    </>
+    </ ThemeProvider>
   );
 };
 
