eeea5a4f5e079bec9fe2d1c90263a3552d7ad347
Kelly <kellytso04@gmail.com>
2021-12-11 14:07:36 -0500

    Finalizes styling

--- pages/login.js
+++ pages/login.js
@@ -4,73 +4,115 @@ import {
   Box,
   Stack,
   Switch,
+  Button,
   Typography,
   TextField,
   Fab
 } from '@mui/material';
 import { Google, Facebook } from '@mui/icons-material';
-import { Theme } from '../components/Theme.js';
+import { Theme, Fonts } from '../components/Theme.js';
 
-// const AntSwitch = styled(Switch)((theme) => {
-//   width: '40px',
-//   height: '15px',
-//   backgroundColor: theme.primary.dark,
-//   color: theme.primary.light,
-// });
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 80%;
+  border: #343a40 5px solid;
+  padding: 10px;
+`
+
+const SwitchContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-content: center;
+  width: 20%;
+`
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 40%;
+  height: 10%;
+`
+
+const FabContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
 
 const Login = () => {
-  const [ signInView, setSignInView ] = useState(true);
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ signUpView, setSignUpView ] = useState(true);
 
-  const handleChange = () => {
-    setSignInView(!signInView);
+  const handleSwitchChange = () => {
+    setSignUpView(!signUpView);
+  }
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
   }
 
   return (
-    <>
-      <Box
-        component='form'
-        // sx='?'
-      >
-        <div>
-          Welcome {signInView ? 'back ' : null}to <b>Marxet</b>
+    <LoginContainer className='login-container' style={{fontFamily: Fonts.heading}}>
+        <div style={{fontSize: '50px'}}>
+          Welcome {signUpView ? 'back ' : null}to
+          <span className='logo' style={{fontFamily: Fonts.logo, color: Theme.secondary.main}}> Marxet</span>
         </div>
-        <Stack direction='row' spacing={1} alignItems='center' >
-          <Typography>Sign in</Typography>
+        <SwitchContainer >
+          <span style={{marginTop: '7px'}}>Sign in</span>
           <Switch
-            onChange={handleChange}
-            defaultChecked
+            onChange={handleSwitchChange}
+            classes={{
+              track: Theme.primary.main
+            }}
             inputProps={{ 'aria-label': 'ant design' }} />
-          <Typography>Sign up</Typography>
-        </ Stack>
-        <br />
-        <Stack direction='column' spacing={1} alignItems='center' >
+          <span style={{marginTop: '7px'}}>Sign up</span>
+        </ SwitchContainer>
+        <InputContainer >
           <TextField
             id='username-field'
             variant='outlined'
             label='Username or E-mail'
-            required='true'
+            required
+            color='secondary'
+            onChange={handleUsernameChange}
+            sx={{paddingBottom: '5px'}}
           />
           <TextField
             id='password-field'
             variant='outlined'
             label='Password'
-            required='true'
+            required
+            color='secondary'
+            onChange={handlePasswordChange}
+            value={password}
+            sx={{paddingBottom: '5px'}}
           />
-        </ Stack>
-        <br />
+          <Button variant='outlined' color='secondary' sx={{paddingTop: '5px'}}>
+            Go
+          </Button>
+        </ InputContainer>
         <div>
-          Sign {signInView ? 'in' : 'up'} with
-          <Stack direction='row' spacing={1} alignItems='center' >
-            <Fab id='google-login' >
+          <div style={{marginBottom: '15px'}}>Sign {signUpView ? 'in' : 'up'} with</div>
+          <FabContainer>
+            <Fab id='google-login' color='secondary' style={{marginRight: '5px'}}>
               <Google />
             </Fab>
-            <Fab id='facebook-login' >
+            <br />
+            <Fab id='facebook-login' color='secondary' style={{marginLeft: '5px'}}>
               <Facebook />
             </Fab>
-          </ Stack>
+          </FabContainer>
         </div>
-      </ Box>
-    </>
+    </LoginContainer>
   )
 }
 
