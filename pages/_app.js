3bd222dbc6f157c152fd806383af737febe31943
Kelly <kellytso04@gmail.com>
2021-12-15 09:31:30 -0500

    Removes extra code blocks

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,13 @@
-import { Box, createTheme, ThemeProvider } from '@mui/material';
-import Link from 'next/link';
-import '../public/styles.css';
-import Header from '../components/Header';
-import firebase from '@services/firebase';
-
-const Theme = createTheme({
-  palette: {
-    primary: {
-      light: '#ffffff',
-      main: '#c7d6d5',
-      dark: '#667574',
-      contrastText: '#667574',
-    },
-    secondary: {
-      light: '#ffffff',
-      main: '#801f12',
-      dark: '#343a40',
-      contrastText: '#ffffff',
-    },
-    info: {
-      main: '#dbd5d5',
-    },
-    success: {
-      main: '#d1e6e4',
-    },
-    error: {
-      main: '#c62828',
-    },
-    warning: {
-      main: '#801f12',
-    },
-  },
-});
-
+import Link from "next/link";
+import firebase from "@services/firebase";
 import { useEffect } from "react";
-
+import { Box, createTheme, ThemeProvider } from "@mui/material";
 import "../public/styles.css";
-// import SearchBar from "../components/SearchBar";
 import Header from "../components/Header";
-import SearchPage from "../features/Search/SearchPage.js";
+import SearchBar from "../components/SearchBar";
+import Header from "../components/Header";
 import AddListing from "../components/modals/addListingModal.js";
+import SearchPage from "../features/Search/SearchPage.js";
 
 const Theme = createTheme({
   palette: {
@@ -76,13 +44,13 @@ const App = ({ Component, pageProps }) => {
       <Box sx={{ flexGrow: 1 }}>
         <Header />
       </Box>
-      <Link href='/products/1'>
+      <Link href="/products/1">
         <a>Search</a>
       </Link>
-      <Link href='/'>
+      <Link href="/">
         <a>Home</a>
-      </Link>{' '}
-      <Link href='/user/1'>
+      </Link>{" "}
+      <Link href="/user/1">
         <a>User 1</a>
       </Link>
       <Component {...pageProps} />
