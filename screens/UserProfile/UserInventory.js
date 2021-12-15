f308fb9c8c20f3020370a68a5173117165b974df
Arun Bhardwaj <arun.b.95@gmail.com>
2021-12-15 15:35:00 -0500

    stuff

--- screens/UserProfile/UserInventory.js
+++ screens/UserProfile/UserInventory.js
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import { Box } from '@mui/material';
 import ItemCard from '../../components/ItemCard/index';
+import styled from 'styled-components';
 
 const UserInventory = () => {
   const [inventory, setInventory] = useState([]);
 
   return (
-    <Box>
+    <StyledBox>
       UserInventory
       <ItemCard />
-    </Box>
+    </StyledBox>
   );
 };
 
 export default UserInventory;
+
+const StyledBox = styled(Box)`
+  outline: 1px solid blue;
+`
