1cfd07eecd42b700480c19174e4b3eeb83892912
Kelly <kellytso04@gmail.com>
2021-12-15 11:28:43 -0500

    Builds out login functionality

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,18 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useState } from "react";
+import styled from "styled-components";
 import {
   Box,
   Stack,
   Switch,
   Button,
-  Typography,
   TextField,
+  InputAdornment,
+  IconButton,
   Fab
-} from '@mui/material';
-import { Google, Facebook } from '@mui/icons-material';
-import { Theme, Fonts } from '../components/Theme.js';
-import { register, signIn, getUserInfo } from '../src/helper.js';
+} from "@mui/material";
+import { Google, Facebook, Visibility } from "@mui/icons-material";
+import { Theme, Fonts } from "../components/Theme.js";
+import { register, signIn, getUserInfo } from "../src/helper.js";
 
 const LoginContainer = styled.div`
   display: flex;
@@ -45,13 +46,18 @@ const FabContainer = styled.span`
 `;
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const [signUpView, setSignUpView] = useState(true);
 
   const handleSwitchChange = () => {
     setSignUpView(!signUpView);
+  };
+
+  const handleVisibleClick = () => {
+    setPasswordVisible(!passwordVisible);
   };
 
   const handleUsernameChange = (e) => {
@@ -64,19 +70,19 @@ const Login = () => {
 
   const handleConfirmPasswordChange = (e) => {
     setConfirmPassword(e.target.value);
-  }
+  };
 
   const handleRegister = (e) => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
-      alert('Your passwords do not match. Please try again.');
-      setPassword('');
-      setConfirmPassword('');
+      alert("Your passwords do not match. Please try again.");
+      setPassword("");
+      setConfirmPassword("");
     } else {
       register(username, password);
-      setPassword('');
-      setConfirmPassword('');
+      setPassword("");
+      setConfirmPassword("");
       handleSwitchChange();
     }
   };
@@ -85,84 +91,98 @@ const Login = () => {
     e.preventDefault();
 
     signIn(username, password);
-    setPassword('');
+    setPassword("");
   };
 
   return (
     <LoginContainer
-      className='login-container'
+      className="login-container"
       style={{ fontFamily: Fonts.heading }}
     >
-      <div style={{ fontSize: '40px' }}>
-        Welcome {signUpView ? 'back ' : null}to
+      <div style={{ fontSize: "40px" }}>
+        Welcome {signUpView ? "back " : null}to
         <span
-          className='logo'
+          className="logo"
           style={{ fontFamily: Fonts.logo, color: Theme.secondary.main }}
         >
-          {' '}
+          {" "}
           Marxet
         </span>
       </div>
-      <SwitchContainer style={{marginBottom: '30px'}} >
-        <span style={{ marginTop: '7px' }}>Sign in</span>
+      <SwitchContainer style={{marginBottom: "30px"}} >
+        <span style={{ marginTop: "7px" }}>Sign in</span>
         <Switch
           onChange={handleSwitchChange}
           classes={{
             track: Theme.primary.main,
           }}
-          inputProps={{ 'aria-label': 'ant design' }}
+          inputProps={{ "aria-label": "ant design" }}
         />
-        <span style={{ marginTop: '7px' }}>Sign up</span>
+        <span style={{ marginTop: "7px" }}>Sign up</span>
       </SwitchContainer>
       <InputContainer >
         <TextField
-          id='username-field'
-          variant='outlined'
-          label='Username or e-mail'
-          color='secondary'
+          id="username-field"
+          variant="outlined"
+          label="Username or e-mail"
+          color="secondary"
           required
           onChange={handleUsernameChange}
-          sx={{ paddingBottom: '5px' }}
+          sx={{ paddingBottom: "5px" }}
         />
         <TextField
-          id='password-field'
-          type='password'
-          variant='outlined'
-          label='Password'
-          color='secondary'
+          id="password-field"
+          type={passwordVisible ? "text" : "password"}
+          variant="outlined"
+          label="Password"
+          value={password}
+          color="secondary"
           required
           onChange={handlePasswordChange}
           value={password}
-          sx={{ paddingBottom: '5px' }}
+          sx={{ paddingBottom: "5px" }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle-password-visible"
+                  onClick={handleVisibleClick}
+                >
+                  <Visibility />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
         />
         { !signUpView ? <TextField
-          id='confirm-password-field'
-          type='password'
-          variant='outlined'
-          label='Confirm your password'
-          color='secondary'
+          id="confirm-password-field"
+          type={passwordVisible ? "text" : "password"}
+          variant="outlined"
+          label="Confirm your password"
+          value={confirmPassword}
+          color="secondary"
           required
           onChange={handleConfirmPasswordChange}
           value={confirmPassword}
-          sx={{ paddingBottom: '5px' }}
+          sx={{ paddingBottom: "5px" }}
         /> : null }
         <Button
-          variant='outlined'
-          color='secondary'
-          sx={{ paddingTop: '5px' }}
-          type='submit'
+          variant="outlined"
+          color="secondary"
+          sx={{ paddingTop: "5px" }}
+          type="submit"
           onClick={signUpView ? handleSignIn : handleRegister}
         >
           Go
         </Button>
       </InputContainer>
-      <div style={{ marginBottom: '15px', marginTop: '50px'}}>
-        Sign {signUpView ? 'in' : 'up'} with
+      <div style={{ marginBottom: "15px", marginTop: "50px"}}>
+        Sign {signUpView ? "in" : "up"} with
         <br />
         <Fab
-          id='google-login'
-          color='secondary'
-          style={{marginTop: '10px'}}
+          id="google-login"
+          color="secondary"
+          style={{marginTop: "10px"}}
         >
           <Google />
         </Fab>
