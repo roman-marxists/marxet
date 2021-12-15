d85b063ab77b011e3efc6e7348d6a3062cd3b728
Kelly <kellytso04@gmail.com>
2021-12-15 11:28:55 -0500

    Builds out login functionality, redirects user upon login/signup

--- src/helper.js
+++ src/helper.js
@@ -1,30 +1,33 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import Link from 'next/link';
+import Router from 'next/router';
 
 const auth = getAuth();
 
 export const register = (email, password) => {
-  createUserWithEmailAndPassword(auth, ...args)
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in
       const user = userCredential.user;
-      // ...
+      Router.push(`/user/${user}`);
     })
     .catch((error) => {
       console.error(error.code);
       console.error(error.message);
+      alert(error.message);
     });
 }
 
 export const signIn = (email, password) => {
-  signInWithEmailAndPassword(auth, ...args)
+  signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in
+      // User successfully signed in
       const user = userCredential.user;
-      // ...
+      Router.push('/products/1');
     })
     .catch((error) => {
       console.error(error.code);
       console.error(error.message);
+      alert(error.message);
     });
 }
 
@@ -36,8 +39,17 @@ export const getUserInfo = () => {
       const uid = user.uid;
       // ...
     } else {
-      // User is signed out
-      // ...
+      console.error('No user is currently signed in');
     }
   });
+}
+
+export const signUserOut = () => {
+  signOut(auth)
+    .then(() => {
+      // TODO: Redirect them to Search
+    }).catch((error) => {
+      // TODO: Alert
+      alert('Unable to sign you out - ', error.message)
+    });
 }
