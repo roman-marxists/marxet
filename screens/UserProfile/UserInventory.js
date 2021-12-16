3ad595000d4921cde93c33a806b9366552c3934a
Arun Bhardwaj <arun.b.95@gmail.com>
2021-12-16 11:38:34 -0500

    updates UserInventory css

--- screens/UserProfile/UserInventory.js
+++ screens/UserProfile/UserInventory.js
@@ -2,6 +2,15 @@ import { useState } from 'react';
 import { Box } from '@mui/material';
 import ItemCard from '../../components/ItemCard/index';
 import styled from 'styled-components';
+import { useTheme } from '@mui/system';
+// import { styled } from '@mui/material/styles';
+
+// const StyledBox = styled(Box)(({theme}) => {
+//   padding: 1,
+//   backgroundColor: theme.palette.success.main,
+//   border-radius: 5px,
+//   box-shadow: 0 3px 10px #888888,
+// })
 
 const UserInventory = () => {
   const [inventory, setInventory] = useState([]);
@@ -9,6 +18,11 @@ const UserInventory = () => {
   return (
     <StyledBox>
       UserInventory
+      {
+        inventory.map(listing => {
+          <ItemCard />
+        })
+      }
       <ItemCard />
     </StyledBox>
   );
@@ -17,5 +31,10 @@ const UserInventory = () => {
 export default UserInventory;
 
 const StyledBox = styled(Box)`
-  outline: 1px solid blue;
+  /* outline: 1px solid blue; */
+  padding: 10px;
+  background: #F8F8F8;
+  border-radius: 5px;
+  box-shadow: 0 3px 10px #888888;
 `
+
