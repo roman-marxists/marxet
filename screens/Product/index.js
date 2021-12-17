e2159fb973c9e2737ecf88ee0ac59af6ee26dee8
Kelly <kellytso04@gmail.com>
2021-12-16 23:07:52 -0500

    Adds styling

--- screens/Product/index.js
+++ screens/Product/index.js
@@ -7,12 +7,21 @@ import {
   ImageList,
   ImageListItem,
   Typography,
-  Button
+  Button,
+  Divider
 } from "@mui/material";
 import { doGetProductById } from "@api/product";
-
 import OfferModal from "@components/modals/Offer";
 
+/*
+TODO: (Done!)
+☑️ Divide component into two containers
+☑️ DECIDE: Quilt or woven variant for ImageList
+☑️ Create ImageList and map listing's photos into ImageListItems
+☑️ IF there are no images, render the message "Lister has not uploaded any images"
+☑️ Create Stack container for listing's name, category, description, etc.
+☑️ IF the user is not logged in, replace the `Make Offer` button with a `Log in to make an offer` button that redirects user to login page
+*/
 
 const Product = () => {
   const { user } = useAuth();
@@ -35,16 +44,19 @@ const Product = () => {
   }, []);
 
   if (listing !== {}) {
+    console.log('LISTING: ', listing);
     return (
-      <Stack className="listing" id={listingID} direction="row" sx={{border: 'black 10px solid', margin: '15px', height: '85vh'}} >
+      <Stack className="listing" id={listingID} direction="row" sx={{margin: '15px', height: '85vh'}} >
         <ImageList
           className="photos-container"
           variant="quilted"
           sx={{
+            width: 'auto',
             maxHeight: '90vh',
-            width: '50vw',
+            maxWidth: '50vw',
             yOverflow: 'scroll',
-            margin: '15px'
+            margin: '15px',
+            border: '#c7d6d5 2px solid'
           }}
         >
           { listing.photos ? listing.photos.map( (photo, i) => (
@@ -54,11 +66,27 @@ const Product = () => {
           )) : <ImageListItem>Lister has not uploaded any photos</ImageListItem>}
         </ImageList>
         <br />
-        <Stack className="listing-info-container" sx={{justifyContent: 'space-between', border: '#801f12 10px solid', width: '40vw', margin: '15px'}}>
-          <span className='plain-text' style={{fontSize: '40px'}}>{listing.name}</span>
-          <span className='plain-text' style={{fontSize: '25px'}}>{listing.description}</span>
-          {/* Waiting on Category, metadata */}
-          <br />
+        <Stack className="listing-info-container"
+          sx={{
+            justifyContent: 'space-evenly',
+            border: '#801f12 3px solid',
+            borderRadius: '20px',
+            width: '40vw',
+            margin: '15px'
+          }}
+        >
+          <Stack className="listing-text-container" sx={{justifyContent: 'space-between', marginLeft: '10px'}}>
+            <div className='plain-text' style={{fontSize: '60px'}}>{listing.name}</div>
+            <div>Listed by {listing.createdBy ? listing.createdBy : 'an anonymous user'} on {listing.createdAt.substring(0, 10)}</div>
+            { listing.createdAt !== listing.updatedAt ?
+              <div>Last updated on {listing.updatedAt.substring(0,10)}</div>
+              :
+              null
+            }
+            <div className='plain-text' style={{fontSize: '30px'}}>{listing.description}</div>
+            {/* Waiting on Category, metadata */}
+          </Stack>
+          <Divider sx={{alignSelf: 'center', width: '70%'}}/>
           <Stack id='listing-button-container' sx={{alignItems: 'center', marginBottom: '10px'}}>
             { user ?
               <Button variant="outlined" color="secondary" sx={{width: '50%'}}>
@@ -84,15 +112,5 @@ const Product = () => {
     )
   }
 };
-
-/*
-TODO:
-☑️ Divide component into two containers
-☑️ DECIDE: Quilt or woven variant for ImageList
-☑️ Create ImageList and map listing's photos into ImageListItems
-☑️ IF there are no images, render the message "Lister has not uploaded any images"
-☑️ Create Stack container for listing's name, category, description, etc.
-☑️ IF the user is not logged in, replace the `Make Offer` button with a `Log in to make an offer` button that redirects user to login page
-*/
 
 export default Product;
